524a6b3187c954ef8e64faef7decede9a2824984
paladinze <paladinze@hotmail.com>
2017-05-12 12:13:32 -0400

    base commit

---
+++ Source/UnrealTank/UnrealTankGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "UnrealTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALTANK_API AUnrealTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
